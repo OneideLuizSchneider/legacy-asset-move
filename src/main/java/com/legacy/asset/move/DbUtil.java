ab9a671d48c4f4ff096a1ff2f38f9decb526d515
Oneide Luiz Schneider <oneidels@gmail.com>
2021-03-01 21:56:33 -0300

    Refactored interaction with the database and Logs

--- src/main/java/com/legacy/asset/move/DbUtil.java
+++ src/main/java/com/legacy/asset/move/DbUtil.java
@@ -2,13 +2,24 @@ package com.legacy.asset.move;
 
 import com.legacy.asset.move.config.DataSource;
 import com.legacy.asset.move.dto.Image;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Slf4j
 public class DbUtil {
 
+    private static final Dotenv dotenv = Dotenv.load();
+
+    /**
+     * <p>
+     * This method will update the database with the new bucket/path
+     * </p>
+     */
     public void execute(Image imageDto) {
         String updateSql = "update `production-db`.images set path = ? where id = ?";
         try (Connection conn = DataSource.getConnection();
@@ -17,9 +28,50 @@ public class DbUtil {
             stmt.setInt(2, imageDto.getId());
             stmt.executeUpdate();
         } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            log.error("Error on method DbUtil.execute-----------------------");
+            log.error(throwables.getMessage());
+            log.error("----------------------------------------------------");
         }
     }
 
+    /**
+     * <p>
+     * This method will validate if at least exists one row on the db
+     * </p>
+     */
+    public static boolean exists() {
+        String sqlCount = "select count(id) as total from `production-db`.images where path not like 'avatar_%' and id % ? = ?";
+        try (Connection conn = DataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlCount)) {
+            stmt.setInt(1, Integer.parseInt(dotenv.get("BOT_TOTAL")));
+            stmt.setInt(2, Integer.parseInt(dotenv.get("BOT_NUMER")));
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            log.info("Total: " + rs.getInt("total"));
+            return rs.getInt("total") > 0;
+        } catch (SQLException throwables) {
+            log.error("Error on method DbUtil.exists-----------------------");
+            log.error(throwables.getMessage());
+            log.error("----------------------------------------------------");
+        }
+        return false;
+    }
+
+    public static ResultSet getImages() {
+        String sqlImages = "select id, path from `production-db`.images where path like ? and id % ? = ? limit ?";
+        try (Connection conn = DataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sqlImages)) {
+            stmt.setString(1, dotenv.get("OLD_PATH") + "%");
+            stmt.setInt(2, Integer.parseInt(dotenv.get("BOT_TOTAL")));
+            stmt.setInt(3, Integer.parseInt(dotenv.get("BOT_NUMER")));
+            stmt.setInt(4, Integer.parseInt(dotenv.get("SQL_LIMIT")));
+            return stmt.executeQuery();
+        } catch (SQLException throwables) {
+            log.error("Error on method DbUtil.getImages-----------------------");
+            log.error(throwables.getMessage());
+            log.error("-------------------------------------------------------");
+        }
+        return null;
+    }
 
 }
--- src/main/java/com/legacy/asset/move/Migration.java
+++ src/main/java/com/legacy/asset/move/Migration.java
@@ -1,59 +1,46 @@
 package com.legacy.asset.move;
 
 import com.legacy.asset.move.config.ConnectionS3;
-import com.legacy.asset.move.config.DataSource;
 import com.legacy.asset.move.dto.Image;
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+@Slf4j
 public class Migration {
 
     private static final Dotenv dotenv = Dotenv.load();
 
+    /**
+     * <p>
+     * This method validate and starts the migration
+     * </p>
+     */
     public void execute() {
-        while (exists()) {
+        while (DbUtil.exists()) {
             try {
                 this.start();
             } catch (SQLException throwables) {
-                System.err.println("Error on method start------------------");
-                System.err.println(throwables.getMessage());
-                System.err.println("---------------------------------------");
+                log.error("Error on method Migration.start--------");
+                log.error(throwables.getMessage());
+                log.error("---------------------------------------");
             }
         }
     }
 
-    private boolean exists() {
-        String sqlCount = "select count(id) as total from `production-db`.images where path not like 'avatar_%' and id % ? = ?";
-        try (Connection conn = DataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sqlCount)) {
-            stmt.setInt(1, Integer.parseInt(dotenv.get("BOT_TOTAL")));
-            stmt.setInt(2, Integer.parseInt(dotenv.get("BOT_NUMER")));
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
-            return rs.getInt("total") > 0;
-        } catch (SQLException throwables) {
-            System.err.println("Error on method exists-----------------------");
-            System.err.println(throwables.getMessage());
-            System.err.println("---------------------------------------------");
-        }
-        return false;
-    }
-
+    /**
+     * <p>
+     * This method starts the migration and threads
+     * to increase the threads, just need to change the .env file and restart
+     * </p>
+     */
     private void start() throws SQLException {
-        String sqlImages = "select id, path from `production-db`.images where path not like 'avatar_%' and id % ? = ? limit ?";
-        try (Connection conn = DataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sqlImages)) {
-            stmt.setInt(1, Integer.parseInt(dotenv.get("BOT_TOTAL")));
-            stmt.setInt(2, Integer.parseInt(dotenv.get("BOT_NUMER")));
-            stmt.setInt(3, Integer.parseInt(dotenv.get("THREADS")));
-            ResultSet rs = stmt.executeQuery();
-
+        ResultSet rs = DbUtil.getImages();
+        if (rs != null) {
             ThreadPoolExecutor executor = getThreadPool();
 
             while (rs.next()) {
@@ -79,9 +66,11 @@ public class Migration {
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException ex) {
-                    System.out.println(ex.getMessage());
+                    log.error(ex.getMessage());
                 }
             }
+
+            rs.close();
         }
     }
 
