831c4b1de36aa7ec912e191907fe25ea4b25fd24
Oneide Luiz Schneider <oneidels@gmail.com>
2021-03-02 22:12:32 -0300

    refactor threadpool

--- src/main/java/com/legacy/asset/move/Migration.java
+++ src/main/java/com/legacy/asset/move/Migration.java
@@ -1,12 +1,13 @@
 package com.legacy.asset.move;
 
-import com.legacy.asset.move.config.ConnectionS3;
 import com.legacy.asset.move.dto.Image;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -14,6 +15,11 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Migration {
 
     private static final Dotenv dotenv = Dotenv.load();
+
+    private ThreadPoolExecutor executor;
+
+    public Migration() {
+    }
 
     /**
      * <p>
@@ -23,7 +29,11 @@ public class Migration {
     public void execute() {
         while (DbUtil.exists()) {
             try {
-                this.start();
+                this.startThreads();
+                this.start(
+                        this.getImages()
+                );
+                this.shutdownThreads();
             } catch (SQLException throwables) {
                 log.error("Error on method Migration.start--------");
                 log.error(throwables.getMessage());
@@ -34,15 +44,13 @@ public class Migration {
 
     /**
      * <p>
-     * This method starts the migration and threads
-     * to increase the threads, just need to change the .env file and restart
+     * This method will return a list of images to migrate
      * </p>
      */
-    private void start() throws SQLException {
+    private List<Image> getImages() throws SQLException {
+        List<Image> imageList = new ArrayList<>();
         ResultSet rs = DbUtil.getImages();
         if (rs != null) {
-            ThreadPoolExecutor executor = getThreadPool();
-
             while (rs.next()) {
                 Image dto = new Image(
                         rs.getInt("id"),
@@ -52,30 +60,43 @@ public class Migration {
                                 dotenv.get("NEW_PATH")
                         )
                 );
-                executor.submit(() -> {
-                    new ConnectionS3().copyObject(
-                            dto.getOldPath(),
-                            dto.getNewPath()
-                    );
-                    new DbUtil().execute(dto);
-                });
+                imageList.add(dto);
             }
-
-            executor.shutdown();
-            while (!executor.isTerminated()) {
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException ex) {
-                    log.error(ex.getMessage());
-                }
-            }
-
             rs.close();
         }
+        return imageList;
+    }
+
+    /**
+     * <p>
+     * This method starts the migration and threads
+     * to increase the threads, just need to change the .env file and restart
+     * </p>
+     */
+    private void start(List<Image> imageList) {
+        imageList.forEach((dto) -> {
+            //this will starts all the threads base on rows returned from the db
+            executor.execute(new Worker(dto));
+        });
     }
 
     private ThreadPoolExecutor getThreadPool() {
         return (ThreadPoolExecutor) Executors.newFixedThreadPool(Integer.parseInt(dotenv.get("THREADS")));
+    }
+
+    private void startThreads() {
+        this.executor = getThreadPool();
+    }
+
+    private void shutdownThreads() {
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                log.error(ex.getMessage());
+            }
+        }
     }
 
 }
